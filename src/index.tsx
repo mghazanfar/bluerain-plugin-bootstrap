fa477a26a3a52f331f4409fc95ea3460b86061d8
amnajunaid <amna.junaid@blueeast.com>
2017-12-27 10:52:29 +0500

    Initial Project structure

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import { Plugin ,bluerain} from '@blueeast/bluerain-os';
+
+
+/**
+ * Bootstrap Plugin
+ * @property {string} pluginName "BootstrapPlugin"
+ * @property {string} slug "bootstrap"
+ */
+class BootstrapPlugin extends Plugin {
+
+	static pluginName = 'BootstrapPlugin';
+	static slug = 'bootstrap';
+
+	static initialize(config = {}, ctx:bluerain) {
+
+		let theme = ctx.Configs.get('theme');
+		if (!theme) {
+			theme = {};
+		}
+	}
+}
+
+export default BootstrapPlugin;
