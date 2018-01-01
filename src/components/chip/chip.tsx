e7facadc95da2f2c1ef6856a2089e494e196b23b
ghazaltaimur <ghazal.taimur@blueeast.com>
2018-01-01 18:20:16 +0500

    chip in bootstrap

--- src/components/chip/chip.tsx
+++ src/components/chip/chip.tsx
@@ -3,7 +3,6 @@
  */
 import * as React from 'react';
 import { Badge } from 'reactstrap';
-import { FormattedMessage } from 'react-intl';
 import { IChip } from '@blueeast/bluerain-ui-interfaces/Components/Chip';
 
 /**
@@ -11,8 +10,12 @@ import { IChip } from '@blueeast/bluerain-ui-interfaces/Components/Chip';
  * @param {string} props.style Get the style object
  * @param {string} props.children Content of the Chip
  */
-const Chip = (props: IChip) => {
-    return (<Badge style={{...props.style}} pill color="primary" {...props}>{props.children}</Badge>);
+
+export interface IChipProps {
+    color?: string;
+}
+const Chip = (props: IChip & IChipProps) => {
+    return (<Badge style={{...props.style}} pill color={props.color} {...props}>{props.children}</Badge>);
 
 };
 export default Chip;
--- src/components/chip/test/chip.test.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Ghazal Taimur on 12/29/17.
- */
-import * as React from 'react';
-import Chip from '../chip';
-import  BR  from '@blueeast/bluerain-os';
-BR.boot({renderApp:false});
