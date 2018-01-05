bc733c62c8b4dc2f0ebc21569320e724e6f0948d
haris_ramzan <haris.ramzan@blueeast.com>
2018-01-05 15:27:18 +0500

    Resolved map test

--- test/map.test.tsx
+++ test/map.test.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import { create } from 'react-test-renderer';
 import Map from '../src/components/map/Map.component';
 
-test('Map component with default values', () => {
+test('Map component with width', () => {
     const map = create(
         <Map
             zoom={12}
             height={'100px'}
             width={'500px'}
+            region={{ latitude: 31.3892751, longitude: 74.1502481 }}
         />
     ).toJSON();
-    expect(map.props.width).toEqual(undefined);
+    expect(map.props.style.width).toEqual('500px' );
     expect(map).toMatchSnapshot();
 });
 
-test('Map component with region', () => {
+test('Map component with height', () => {
     const map = create(
         <Map height={'600px'} />
     ).toJSON();
-    expect(map.props.style).toEqual({ height:'600px' });
+    expect(map.props.style.height).toEqual('600px');
     expect(map).toMatchSnapshot();
 });
