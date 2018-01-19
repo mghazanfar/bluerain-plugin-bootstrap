ca4fe6d9948b41f4143871ef2c2b9810a47edcc6
m_anser <anser.naseer@blueeast.com>
2018-01-19 10:18:07 +0500

    Build recreated

--- src/components/FooterBar/footerBar.stories.tsx
+++ src/components/FooterBar/footerBar.stories.tsx
@@ -7,7 +7,7 @@ import { withKnobs, text, boolean, object} from '@storybook/addon-knobs';
 const stories = storiesOf('Footer Bar', module);
 
 stories.add('Footer Icons', () => {
-    const name = text('Icon Name', 'settings');
+    const name = text('Icon Name', 'home');
     const color = text('Icon Color', 'black');
     const size = text('Icon Size', 200);
   return <FooterBarComponent name={name} color={color} size={size} />;
