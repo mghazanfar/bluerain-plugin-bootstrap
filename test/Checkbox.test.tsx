6d7d58fff7230239b8096c94358ab4b06b3edb4f
Ghazanfar <ghazanfar.ali@blueeast.com>
2018-03-03 12:27:42 +0500

    Added Checkbox.test.tsx

--- test/Checkbox.test.tsx
+++ test/Checkbox.test.tsx
@@ -6,13 +6,13 @@ test('when title prop is passed', () => {
     const checkbox = create(
         <Checkbox title="hello" />
     ).toJSON();
-    expect(checkbox.props.placeholder).toEqual('hello');
+    expect(checkbox.props.title).toEqual('hello');
     expect(checkbox).toMatchSnapshot();
 });
 test('when checked prop is passed', () => {
     const checkbox = create(
         <Checkbox checked={true} />
     ).toJSON();
-    expect(checkbox.props.autoFocus).toEqual(true);
+    expect(checkbox.props.checked).toEqual(true);
     expect(checkbox).toMatchSnapshot();
 });
